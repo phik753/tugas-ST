037a3a5b5f61fee4156564b239a0169adc2771ab
phik753 <89512318+phik753@users.noreply.github.com>
2023-11-25 14:30:05 +0700

    revisi layout activity_main

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ServerApplication"
+include(":app")
+include(":clientapplication")
